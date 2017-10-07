c946b9485bd948b3174d607e3f8f4010a488b4f4
Markus Lindelöw <markus.lindelow@smarteye.se>
2017-10-07 13:35:58 +0200

    Add basic support for Windows

--- example/main.c
+++ example/main.c
@@ -1,6 +1,38 @@
 #include <tinycsocket.h>
 
+#include <stdlib.h>
+#include <stdio.h>
+
+int show_error(const char* error_text)
+{
+  fprintf(stderr, error_text);
+  getchar();
+  return -1;
+}
+
 int main(int argc, const char* argv[])
 {
+  if (tinycsocket_init() != TINYCSOCKET_SUCCESS)
+  {
+    return show_error("Could not init tinycsocket");
+  }
+
+  TinyCSocketCtx* socketCtx = NULL;
+  if (tinycsocket_create_socket(&socketCtx) != TINYCSOCKET_SUCCESS)
+  {
+    return show_error("Could not create a socket");
+  }
+
+  if (tinycsocket_close_socket(&socketCtx) != TINYCSOCKET_SUCCESS)
+  {
+    return show_error("Could not close the socket");
+  }
+
+  if (tinycsocket_free() != TINYCSOCKET_SUCCESS)
+  {
+    return show_error("Could not free tinycsocket");
+  }
+  printf("OK");
+  getchar();
 	return 0;
 }
--- src/tinycsocket.h
+++ src/tinycsocket.h
@@ -3,14 +3,25 @@
 #ifndef TINY_C_SOCKETS_H_
 #define TINY_C_SOCKETS_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 static int TINYCSOCKET_SUCCESS = 0;
 static int TINYCSOCKET_ERROR_UNKNOWN = -1;
 static int TINYCSOCKET_ERROR_MEMORY = -2;
 static int TINYCSOCKET_ERROR_INVALID_ARGUMENT = -3;
+static int TINYCSOCKET_ERROR_KERNEL;
 
 typedef void TinyCSocketCtx;
 
+int tinycsocket_init();
+int tinycsocket_free();
 int tinycsocket_create_socket(TinyCSocketCtx** outSocketCtx);
 int tinycsocket_close_socket(TinyCSocketCtx** inoutSocketCtx);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
