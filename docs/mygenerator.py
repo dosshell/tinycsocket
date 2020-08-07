73a655e4867b347ae6e8332496c3a49421dac92c
Markus Lindelöw <markus.lindelow@smarteye.se>
2020-08-07 17:12:37 +0200

    Generalize mygen

--- docs/mygenerator.py
+++ docs/mygenerator.py
@@ -1,44 +1,51 @@
 import xml.etree.ElementTree as ET
 import collections
+from pathlib import Path
 
-file = r"./_doxyxml/tinycsocket_8h.xml"
-FunctionData = collections.namedtuple('FunctionData', ['name', 'refid', 'description'])
+FunctionData = collections.namedtuple(
+    'FunctionData', ['name', 'refid', 'description'])
 
 
-def get_function_descriptons():
+def get_function_descriptons(doxygen_folder: Path):
+    index_file = Path(doxygen_folder, 'index.xml')
+    index_root = ET.parse(index_file)
+    index_files = index_root.findall(".//*[@kind='file']")
+    all_files = [Path(doxygen_folder, x.get('refid') + '.xml') for x in index_files]
+
     m = []
-    tree = ET.parse(file)
-    root = tree.getroot()
-    fn = root.findall(".//*[@kind='function']")
+    for file in all_files:
+        tree = ET.parse(file)
+        root = tree.getroot()
+        fn = root.findall(".//*[@kind='function']")
 
-    for f in fn:
-        d = f.find('name').text
-        b = f.find('briefdescription').find('para')
-        refid = f.get('id')
+        for f in fn:
+            d = f.find('name').text
+            b = f.find('briefdescription').find('para')
+            refid = f.get('id')
 
-        if b is not None:
-            txt = b.text
-        else:
-            txt = ""
-        m.append(FunctionData(d, refid, txt))
+            if b is not None:
+                txt = b.text
+            else:
+                txt = ""
+            m.append(FunctionData(d, refid, txt))
     return m
 
 
-def write_table(m: dict):
-    h = f""".. csv-table::
+def write_function_index(m, file: Path):
+    file.parent.mkdir(parents=True, exist_ok=True)
+    f = open(file, "w")
+    f.write(""".. csv-table::
     :header: "symbol", "description"
     :widths: 20, 20
 
-"""
-    f = open("function_index.rst", "w+")
-    f.write(h)
+""")
     for n in m:
         f.write(f'''    "`{n.name} <#{n.refid}>`_", "{n.description}"\n''')
 
 
 def run():
-    m = get_function_descriptons()
-    write_table(m)
+    m = get_function_descriptons(Path('./_doxyxml/'))
+    write_function_index(m, Path('./_mybuild/function_index.rst'))
 
 
 if __name__ == '__main__':
