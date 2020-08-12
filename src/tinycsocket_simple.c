5375dba60359b0d07e76943108ecb0fa20a0c61a
Markus Lindelöw <markus.lindelow@smarteye.se>
2020-08-12 10:11:35 +0200

    Fix warning for 64bit size_t check

--- src/tinycsocket_simple.c
+++ src/tinycsocket_simple.c
@@ -206,9 +206,11 @@ int tcs_simple_send_netstring(tcs_socket socket_ctx, uint8_t* buffer, size_t buf
     if (buffer == NULL || buffer_length == 0)
         return TCS_ERROR_INVALID_ARGUMENT;
 
+#if SIZE_MAX > 0xffffffffffffffffULL
     // buffer_length bigger than 64 bits? (size_t can be bigger on some systems)
     if (buffer_length > 0xffffffffffffffffULL)
         return TCS_ERROR_INVALID_ARGUMENT;
+#endif
 
     int header_length = 0;
     char netstring_header[21] = {0};
