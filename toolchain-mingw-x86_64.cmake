816f74cfc5294c23de18800f71c30798b6d5d79a
Markus Lindelöw <markus@lindeloew.se>
2021-07-21 11:31:39 +0200

    Add MinGW to CI

---
+++ toolchain-mingw-x86_64.cmake
@@ -0,0 +1,6 @@
+# Used when compiling
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR AMD64)
+set(CMAKE_C_COMPILER /usr/bin/x86_64-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER /usr/bin/x86_64-w64-mingw32-g++)
+set(CMAKE_AR /usr/bin/x86_64-w64-mingw32-ar)
