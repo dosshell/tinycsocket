395fea2defc5ee5378e1e4fcf4894082acda5199
Markus Lindelöw <markus@lindeloew.se>
2021-07-22 12:56:42 +0200

    Add more MinGW tests

---
+++ toolchain_mingw_x86-64_winblue.cmake
@@ -0,0 +1,7 @@
+# Used when cross-compiling to windows
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR AMD64)
+set(CMAKE_C_COMPILER "/usr/bin/x86_64-w64-mingw32-gcc")
+set(CMAKE_CXX_COMPILER "/usr/bin/x86_64-w64-mingw32-g++")
+set(CMAKE_AR "/usr/bin/x86_64-w64-mingw32-ar")
+add_definitions(-DWINVER=0x603)
