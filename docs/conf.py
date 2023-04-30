e9329c0de4212196adf78decb1957aef0a509ac1
Markus Lindelöw <markus@lindeloew.se>
2023-04-30 13:40:51 +0200

    Fix sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -1,38 +1,37 @@
-# -- Path setup --------------------------------------------------------------
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
 from subprocess import call
-import mygenerator
+
+import pypandoc
+
+sys.path.insert(0, os.path.abspath("."))
+import reference_generator
 
 
-# -- Project information -----------------------------------------------------
-project = u'TinyCSocket'
-copyright = u'2020, Markus Lindelöw'
-author = u'Markus Lindelöw'
+# Project information
+project = "TinyCSocket"
+copyright = "2020, Markus Lindelöw"
+author = "Markus Lindelöw"
 
-
-# -- General configuration ---------------------------------------------------
-master_doc = 'index'
+# General configuration
+master_doc = "index"
 extensions = []
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-
-
-# -- Options for HTML output -------------------------------------------------
-html_theme = 'classic'
-html_logo = 'static/logo.png'
-html_favicon = 'static/favicon.ico'
+# html options
+html_theme = "classic"
+html_logo = "static/logo.png"
+html_favicon = "static/favicon.ico"
 html_show_sphinx = False
 html_show_copyright = False
-html_title = 'Docs'
-html_static_path = ['static']
-html_sidebars = {
-    '**': ['localtoc.html', 'searchbox.html']
-}
+html_title = "Docs"
+html_static_path = ["static"]
+html_sidebars = {"**": ["localtoc.html", "searchbox.html"]}
 html_use_index = False
 
-# -- Generate doxygen xml ------
-call('doxygen')
-mygenerator.run()
+# Run generators
+os.makedirs("_mybuild", exist_ok=True)
+pypandoc.convert_file("../README.md", "rst", outputfile="_mybuild/README.rst")
+call("doxygen")
+reference_generator.generate_references()
