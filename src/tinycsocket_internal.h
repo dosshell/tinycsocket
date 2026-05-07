d50e11c195e8ee8beb579509c72b33bb5bf27d5b
Markus Lindelöw <markus@lindeloew.se>
2026-05-07 12:22:26 +0200

    Move license text to common

--- src/tinycsocket_internal.h
+++ src/tinycsocket_internal.h
@@ -24,26 +24,7 @@
 #define TINYCSOCKET_INTERNAL_H_
 
 static const char* const TCS_VERSION_TXT = "v0.3.77";
-static const char* const TCS_LICENSE_TXT =
-    "Copyright 2018 Markus Lindelöw\n"
-    "\n"
-    "Permission is hereby granted, free of charge, to any person obtaining a copy "
-    "of this software and associated documentation files(the \"Software\"), to deal "
-    "in the Software without restriction, including without limitation the rights "
-    "to use, copy, modify, merge, publish, distribute, sublicense, and / or sell "
-    "copies of the Software, and to permit persons to whom the Software is "
-    "furnished to do so, subject to the following conditions:\n"
-    "\n"
-    "The above copyright notice and this permission notice shall be included in all "
-    "copies or substantial portions of the Software.\n"
-    "\n"
-    "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR "
-    "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, "
-    "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE "
-    "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER "
-    "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, "
-    "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE "
-    "SOFTWARE.";
+extern const char* const TCS_LICENSE_TXT;
 
 /*
 * List of all functions in the library:
